005c8d626a6e5c2b8b7e99f277fea0036f8d7d08
Carlitos <carlitosdutan@pursuit.org>
2024-10-18 19:48:38 -0400

    created a json file that divides players based on their positions

--- premier-league-project.py
+++ premier-league-project.py
@@ -3,11 +3,40 @@
 import requests
 import json
 
-response = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/").json()
+main_api = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/").json()
 
-players = response['elements']
+players = main_api['elements']
 
-top_players = sorted(players, key=lambda player:player["total_points"], reverse=True)[:10]
+teams = main_api['teams']
 
+goalkeepers = []
+defenders = []
+midfielders = []
+forwards = []
 
-print(json.dumps(top_players, indent=4))
+for player in players:
+    if player["element_type"] == 1:
+        goalkeepers.append(player)
+    elif player["element_type"] == 2:
+        defenders.append(player)
+    elif player["element_type"] == 3:
+        midfielders.append(player)
+    elif player["element_type"] == 4:
+        forwards.append(player)
+
+player_data = {
+    "goalkeepers": goalkeepers,
+    "defenders": defenders,
+    "midfielders": midfielders,
+    "forwards": forwards
+}
+
+# Things to note "w" will overwrite, could use this for cron job
+with open("players.json", "w") as json_file:
+    json.dump(player_data, json_file, indent=4)
+
+print(len(player_data["midfielders"]))
+
+    
+
+# print(json.dumps(attackers, indent=4))
